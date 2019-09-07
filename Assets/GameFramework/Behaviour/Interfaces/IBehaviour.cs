9300089ea7d9af450e49dcbf31d39855702ee73c
davidMa88 <davidmae20@gmail.com>
2019-09-07 15:23:12 +0200

    Directory structure

---
+++ Assets/GameFramework/Behaviour/Interfaces/IBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBehaviour
+{
+    event Action<int> onHungry;
+    void CheckCurrentStats(int hungry);
+}
